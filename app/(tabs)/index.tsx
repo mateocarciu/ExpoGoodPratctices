44e64c7e175890750447ab4dab513dcb037f3cdf
Mateo <carciu98@gmail.com>
2025-02-26 23:07:16 +0100

    feat(gestures): add gestures on emoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,13 @@ import { View, StyleSheet } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import { type ImageSource } from 'expo-image'
-
 import Button from '@/components/Button'
 import ImageViewer from '@/components/ImageViewer'
 import IconButton from '@/components/IconButton'
 import CircleButton from '@/components/CircleButton'
 import EmojiPicker from '@/components/EmojiPicker'
 import EmojiList from '@/components/EmojiList'
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import EmojiSticker from '@/components/EmojiSticker'
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
@@ -52,29 +51,31 @@ export default function Index() {
 	}
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.imageContainer}>
-				<ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-				{pickedEmoji && <EmojiSticker imageSize={80} stickerSource={pickedEmoji} />}
-			</View>
-			{showAppOptions ? (
-				<View style={styles.optionsContainer}>
-					<View style={styles.optionsRow}>
-						<IconButton icon='refresh' label='Reset' onPress={onReset} />
-						<CircleButton onPress={onAddSticker} />
-						<IconButton icon='save-alt' label='Save' onPress={onSaveImageAsync} />
+		<GestureHandlerRootView>
+			<View style={styles.container}>
+				<View style={styles.imageContainer}>
+					<ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+					{pickedEmoji && <EmojiSticker imageSize={80} stickerSource={pickedEmoji} />}
+				</View>
+				{showAppOptions ? (
+					<View style={styles.optionsContainer}>
+						<View style={styles.optionsRow}>
+							<IconButton icon='refresh' label='Reset' onPress={onReset} />
+							<CircleButton onPress={onAddSticker} />
+							<IconButton icon='save-alt' label='Save' onPress={onSaveImageAsync} />
+						</View>
 					</View>
-				</View>
-			) : (
-				<View style={styles.footerContainer}>
-					<Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
-					<Button label='Use this photo' onPress={() => setShowAppOptions(true)} />
-				</View>
-			)}
-			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-			</EmojiPicker>
-		</View>
+				) : (
+					<View style={styles.footerContainer}>
+						<Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
+						<Button label='Use this photo' onPress={() => setShowAppOptions(true)} />
+					</View>
+				)}
+				<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+					<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+				</EmojiPicker>
+			</View>
+		</GestureHandlerRootView>
 	)
 }
 
